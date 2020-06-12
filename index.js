e654a99b230c6a0cf9976dbf30513f3644dc3504
Galo Sandoval <galosan92@gmail.com>
2020-06-12 09:23:03 -0700

    over halfway done with MVP

--- index.js
+++ index.js
@@ -208,10 +208,18 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log(artists[0]);
+console.log(artists[2]);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+function changeName(array, index, name){
+  array[index].name = name;
+  return array;
+};
+
+console.log(changeName(artists, 8, 'Vincent Van Gogh'));
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -223,8 +231,12 @@ const artists = [
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
-  }
+  name = array[index].name;
+  id = array[index].id;
+  return `The artist at index ${id} is ${name}`
+}
+
+console.log(getArtistByIndex(artists, 2));
   
   /**
 
@@ -248,9 +260,12 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
-  }
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  return array.length;
+}
+
+console.log(removeArtist(artists, 0))
   
   /**
 
@@ -269,11 +284,21 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should console.log() the new array with information added"*/
 
-function addArtist(/* Code here */){
+const myBio = {
+id: 21,
+name: 'Galo Sandoval', 
+Years: '1992-present',
+genre: 'Web Design', 
+nationality: 'American-Ecuadorian',
+bio: 'use lorem ipsum'
+}
 
-    /* Code here */
+function addArtist(newArr, array){
+  bothArr = newArr.push(array);
+  return bothArr;
+}
 
-  }
+console.log(addArtist(myBio, artists));
 
 /* Task 7: Create a function called lotsOfArt() that takes one argument: 
 
